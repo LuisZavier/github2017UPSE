83c2e6ee329a122395b98a7c4d4af1b204bf59a0
LuisZavier <quiensabe077@gmail.com>
2017-07-19 11:18:07 -0500

    Cualquier cosa

--- src/AlgoritmoNuevo.java
+++ src/AlgoritmoNuevo.java
@@ -4,6 +4,25 @@ public class AlgoritmoNuevo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Algoritmo salvaje :");
+		
+		int i=0;
+		int nota=0;
+		boolean copiaEnElExamen = true;
+		do {
+			System.out.println("Tu nota es " + i);
+			
+			if (i>5) {
+				break;
+			}
+			i++;
+		}while(copiaEnElExamen);
+		
+		
 	}
 
+	public static void imprimirTesis(){
+		System.out.println("Bla bla bla relleno !!\n");
+		System.out.println("Mas lata !!\n");
+		System.out.println("lata ++ !!\n");
+	}
 }
